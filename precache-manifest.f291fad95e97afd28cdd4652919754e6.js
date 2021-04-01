3a608eb6cdcd2c94c07d9a69384e7558b4aeb65e
staniboy <stanley.v@outlook.com>
2021-04-01 20:36:10 +0400

    Updates

---
+++ precache-manifest.f291fad95e97afd28cdd4652919754e6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6114ad861b20878a6582",
+    "url": "/REPO_NAME/css/app.a595f26f.css"
+  },
+  {
+    "revision": "0b99c64aab0be42364896376668addb8",
+    "url": "/REPO_NAME/img/check-circle.0b99c64a.svg"
+  },
+  {
+    "revision": "b85b85cf21ee7c272e43d228e9ac0b2d",
+    "url": "/REPO_NAME/img/circle.b85b85cf.svg"
+  },
+  {
+    "revision": "78f00cdad8f0977e55caeb9f5d155a1e",
+    "url": "/REPO_NAME/img/grip-vertical.78f00cda.svg"
+  },
+  {
+    "revision": "ff078b0f4ec01a27647a21c48df687dc",
+    "url": "/REPO_NAME/img/x.ff078b0f.svg"
+  },
+  {
+    "revision": "fe2bd2b42bfa42ba79920503f07be4f5",
+    "url": "/REPO_NAME/index.html"
+  },
+  {
+    "revision": "6114ad861b20878a6582",
+    "url": "/REPO_NAME/js/app.066cb0bf.js"
+  },
+  {
+    "revision": "2b4765797e1059fd1d40",
+    "url": "/REPO_NAME/js/chunk-vendors.00aea0bc.js"
+  },
+  {
+    "revision": "a64286e98632c54656ea462f1d380c9a",
+    "url": "/REPO_NAME/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/REPO_NAME/robots.txt"
+  }
+]);
