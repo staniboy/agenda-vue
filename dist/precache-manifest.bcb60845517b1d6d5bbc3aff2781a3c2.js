d6e9bf288424f82ceeca4ce3ab16e5f67bf29f4e
staniboy <stanley.v@outlook.com>
2021-04-01 19:39:10 +0400

    first deploy

---
+++ dist/precache-manifest.bcb60845517b1d6d5bbc3aff2781a3c2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0229d035c3f821ab1b29",
+    "url": "/css/app.a595f26f.css"
+  },
+  {
+    "revision": "0b99c64aab0be42364896376668addb8",
+    "url": "/img/check-circle.0b99c64a.svg"
+  },
+  {
+    "revision": "b85b85cf21ee7c272e43d228e9ac0b2d",
+    "url": "/img/circle.b85b85cf.svg"
+  },
+  {
+    "revision": "78f00cdad8f0977e55caeb9f5d155a1e",
+    "url": "/img/grip-vertical.78f00cda.svg"
+  },
+  {
+    "revision": "ff078b0f4ec01a27647a21c48df687dc",
+    "url": "/img/x.ff078b0f.svg"
+  },
+  {
+    "revision": "773e97b0becc2047157cc951a9315666",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0229d035c3f821ab1b29",
+    "url": "/js/app.6cce476d.js"
+  },
+  {
+    "revision": "2b4765797e1059fd1d40",
+    "url": "/js/chunk-vendors.00aea0bc.js"
+  },
+  {
+    "revision": "a64286e98632c54656ea462f1d380c9a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
