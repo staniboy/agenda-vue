a916e7af0d4e19abc90aa2023672698de9840ac2
staniboy <stanley.v@outlook.com>
2021-04-05 03:59:02 +0400

    added dropdown menu and modal

--- src/components/ItemInput.vue
+++ src/components/ItemInput.vue
@@ -1,27 +1,110 @@
 <template>
-  <header class="sticky-top">
-    <nav class="navbar container">
-      <form class="w-100">
-        <div class="input-group">
-          <input
-            ref="input"
-            type="text"
-            class="form-control"
-            placeholder="Enter New Task..."
-            aria-label="New task field"
-            autocomplete="off"
-          />
-          <div class="input-group-append">
-            <button
-              class="btn btn-primary"
-              type="submit"
-              @click.prevent="onAddItem"
-            >
-              Add
-            </button>
-          </div>
+  <!-- Modal -->
+  <div
+    class="modal fade"
+    id="clearListModal"
+    data-bs-backdrop="static"
+    data-bs-keyboard="false"
+    tabindex="-1"
+    aria-labelledby="staticBackdropLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="staticBackdropLabel">Clear this list?</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
         </div>
-      </form>
+        <div class="modal-body">
+          All tasks in this list will be gone! Are you sure?
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+          >
+            Close
+          </button>
+          <button
+            type="button"
+            class="btn btn-danger text-white"
+            data-bs-dismiss="modal"
+            @click="emitOnClearList"
+          >
+            Delete!
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Modal End -->
+  <header class="sticky-top">
+    <div class="nav-menu-col collapse navbar-collapse" id="collapseTarget">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#" @click="emitOnDeleteChecked">
+            <img src="../assets/delete-checked.svg" alt="" /> Delete Checked</a
+          >
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" @click="emitOnResetChecked"
+            ><img src="../assets/reset-checked.svg" alt="" /> Reset Checked</a
+          >
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link"
+            data-bs-toggle="modal"
+            data-bs-target="#clearListModal"
+            href="#"
+            ><img src="../assets/clear-list.svg" alt="" /> Clear List</a
+          >
+        </li>
+      </ul>
+      <span class="version">0.1.0</span>
+    </div>
+    <nav class="navbar px-0 navbar-light bg-white">
+      <div class="d-inline-flex w-100">
+        <button
+          class="navbar-toggler me-2 py-0 bg-light"
+          type="button"
+          data-bs-target="#collapseTarget"
+          data-bs-toggle="collapse"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <form class="w-100">
+          <div class="input-group">
+            <input
+              ref="input"
+              type="text"
+              class="form-control flex-shrink-1"
+              placeholder="..."
+              aria-label="New task field"
+              autocomplete="off"
+            />
+            <div class="input-group-append">
+              <button
+                class="btn btn-primary"
+                type="submit"
+                @click.prevent="emitOnAddItem"
+              >
+                Add
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
     </nav>
   </header>
 </template>
@@ -30,15 +113,7 @@
   import { defineComponent, ref } from "vue";
 
   export default defineComponent({
-    emits: {
-      onAddItem({ content }): boolean {
-        if (content === "") {
-          alert("Please specify task!");
-          return false;
-        }
-        return true;
-      },
-    },
+    emits: ["onAddItem", "onDeleteChecked", "onResetChecked", "onClearList"],
 
     setup(props, { emit }) {
       const input = ref();
@@ -48,14 +123,43 @@
         input.value.focus();
       }
 
-      function onAddItem() {
+      function emitOnAddItem() {
         emit("onAddItem", input.value.value);
         resetInput();
       }
-      return { input, resetInput, onAddItem };
+      function emitOnDeleteChecked() {
+        emit("onDeleteChecked");
+      }
+      function emitOnResetChecked() {
+        emit("onResetChecked");
+      }
+      function emitOnClearList() {
+        emit("onClearList");
+      }
+      return {
+        input,
+        resetInput,
+        emitOnAddItem,
+        emitOnDeleteChecked,
+        emitOnResetChecked,
+        emitOnClearList,
+      };
     },
   });
 </script>
 <style lang="scss">
   @import "../../node_modules/bootstrap/scss/bootstrap.scss";
+
+  .nav-menu-col {
+    a {
+      @extend .py-3;
+      color: black;
+    }
+  }
+  .version {
+    // @extend .text-center;
+    @extend .text-black-50;
+    @extend .p-0;
+    font-size: 0.75em;
+  }
 </style>
