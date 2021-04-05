a67a59768a1432ab3fecd81e64beab36e7123a18
staniboy <stanley.v@outlook.com>
2021-04-05 04:45:11 +0400

    changed icon size to better fix android and theme color

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   publicPath: "",
   pwa: {
-    name: 'Agenda Vue',
-    manifestCrossorigin: 'anonymous',
-    themeColor: '#000000'
+    name: "Agenda Vue",
+    manifestCrossorigin: "anonymous",
+    themeColor: "#FFFFFF",
   },
 };
