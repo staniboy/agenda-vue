1a5e3ce476f0f3e21cc594c5deeb24a016104779
staniboy <stanley.v@outlook.com>
2021-07-12 19:19:47 +0400

    feat: added dummy state to vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,70 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import { List, Item } from "../types";
 
 export default createStore({
   state: {
+    lists: [
+      {
+        id: 0,
+        name: "List ID 0",
+        items: [
+          {
+            id: 0,
+            text: "default item 1",
+            status: false,
+          },
+          {
+            id: 1,
+            text: "default item 2",
+            status: false,
+          },
+          {
+            id: 2,
+            text: " default item 3",
+            status: true,
+          },
+        ],
+      },
+      {
+        id: 1,
+        name: "List ID 1",
+        items: [
+          {
+            id: 0,
+            text: "default item 2 1",
+            status: true,
+          },
+          {
+            id: 1,
+            text: "default item 2 2",
+            status: false,
+          },
+        ],
+      },
+      {
+        id: 3,
+        name: "List ID 3",
+        items: [
+          {
+            id: 0,
+            text: "default item 1",
+            status: true,
+          },
+          {
+            id: 1,
+            text: "default item 2",
+            status: true,
+          },
+          {
+            id: 2,
+            text: "default item 3",
+            status: true,
+          },
+        ],
+      },
+    ] as Array<List>,
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  mutations: {},
+  actions: {},
+  modules: {},
+});
