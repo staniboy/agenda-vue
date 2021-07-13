d2de161775bd9519d22ef1d325512f8837afa6ea
staniboy <stanley.v@outlook.com>
2021-07-13 06:51:04 +0400

    refactor: improved CSS

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,51 +1,50 @@
 <template>
-  <div class="app-list-item">
-    <img
-      class="app-list-item-handle"
-      alt="grip"
-      src="../assets/grip-vertical.svg"
-    />
-    <img
-      class="app-list-item-button"
-      alt="checkbox-unchecked"
-      src="../assets/circle.svg"
-      v-if="!model.status"
-      @click="onToggle"
-    />
-    <img
-      class="app-list-item-button"
-      alt="checkbox-checked"
-      src="../assets/check-circle.svg"
-      v-else
-      @click="onToggle"
-    />
-    <div class="d-flex flex-column justify-content-start flex-grow-1">
-      <div class="d-inline-flex">
-        <div
-          class="app-list-item-content"
-          contenteditable="true"
-          @keydown.enter="onEdit"
-          @blur="onEdit"
-        >
-          {{ model.text }}
-        </div>
-      </div>
-      <div class="app-list-item-info">Added on: {{ model.dateAdded }}</div>
+  <div class="list-item-container">
+    <div class="handle-container">
+      <img class="icon handle" alt="grip" src="../assets/grip-vertical.svg" />
     </div>
-    <img
-      class="app-list-item-delete-button"
-      alt="delete"
-      src="../assets/x.svg"
-      v-if="model.status"
-      @click="onDelete"
-    />
-    <div v-else class="app-list-item-delete-button"></div>
+    <div class="rb-container">
+      <img
+        class="icon"
+        alt="checkbox-unchecked"
+        src="../assets/circle.svg"
+        v-if="!model.status"
+        @click="onToggle"
+      />
+      <img
+        class="icon"
+        alt="checkbox-checked"
+        src="../assets/check-circle.svg"
+        v-else
+        @click="onToggle"
+      />
+    </div>
+    <div class="content-container">
+      <div
+        class="top"
+        contenteditable="true"
+        @keydown.enter="onEdit"
+        @blur="onEdit"
+      >
+        {{ model.text }}
+      </div>
+      <div class="bottom">Added on: {{ model.dateAdded }}</div>
+    </div>
+    <div class="end-container">
+      <img
+        class="icon"
+        alt="delete"
+        src="../assets/x.svg"
+        v-if="model.status"
+        @click="onDelete"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
   import { defineComponent } from "vue";
-import { useStore } from "vuex";
+  import { useStore } from "vuex";
 
   export default defineComponent({
     props: {
@@ -61,7 +60,7 @@ import { useStore } from "vuex";
       listId: {
         type: Number,
         required: true,
-      }
+      },
     },
     setup(props) {
       const store = useStore();
@@ -101,49 +100,29 @@ import { useStore } from "vuex";
 <style lang="scss" scoped>
   @import "../../node_modules/bootstrap/scss/bootstrap.scss";
 
-  .app-list-item {
-    @extend .list-group-item;
-    @extend .py-2;
-    @extend .px-0;
-    @extend .d-flex;
-    @extend .align-items-center;
-    border: none;
-  }
-
-  .app-list-item-button {
-    @extend .me-1;
-    height: 32px;
-    width: 32px;
-  }
-
-  .app-list-item-delete-button {
-    @extend .app-list-item-button;
-    // @extend .ms-auto;
-  }
-
-  .app-list-item-handle {
-    // @extend .my-2;
-    @extend .me-1;
-    height: 32px;
-    width: 32px;
-  }
-
-  .app-list-item-content {
-    @extend .px-2;
-  }
-
-  .app-list-item-info {
-    @extend .app-list-item-content;
-    font-size: 12px;
-    color: grey;
-  }
-
-  .app-list-item-content:focus {
-    background-color: black;
-    color: white;
-  }
-  .app-list-item-content-end {
-    @extend .flex-grow-1;
-    @extend .h-100;
+  .list-item-container {
+    display: flex;
+    align-items: center;
+    margin: 1em 0;
+    .handle-container {
+      width: 32px;
+    }
+    .rb-container {
+      width: 32px;
+    }
+    .content-container {
+      flex-grow: 1;
+      margin-left: .8em;
+      .bottom {
+        color: grey;
+        font-size: 0.8em;
+      }
+    }
+    .end-container {
+      width: 32px;
+    }
+    .icon {
+        width: 100%;
+      }
   }
 </style>
